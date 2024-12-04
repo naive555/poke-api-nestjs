b35ec22a0766e3517ff69f68a41f08a4c2f09a09
naive555 <non.naive@gmail.com>
2024-12-04 14:54:35 +0700

    [add] loggin interceptor to profiling api performance.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggingInterceptor } from './middleware/loggin.interceptor';
 import { AppConfigModule } from './config/app.config.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
@@ -9,6 +11,12 @@ import { PokemonModule } from './pokemon/pokemon.module';
 @Module({
   imports: [AppConfigModule, AuthModule, UserModule, PokemonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
