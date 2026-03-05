0a733fd43bbb6309bf70ca4323cb7ccee6d4c5f0
Non Ninkham <non.n@sms2pro.com>
2026-03-05 10:14:20 +0700

    feat: migrate to psql

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,16 +1,27 @@
 import { registerAs } from '@nestjs/config';
 
-import { ISqlConfig } from '../utility/common.interface';
-
 export default registerAs<ISqlConfig>('database', () => ({
   type: process.env.DATABASE_TYPE || 'mysql',
   host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_DATABASE,
+  database: process.env.DATABASE_DB,
   port: +process.env.DATABASE_PORT,
   logging: process.env.DATABASE_LOGGING === 'true',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrationsRun: process.env.DATABASE_MIGRATIONS_RUN === 'true',
   synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
 }));
+
+export interface ISqlConfig {
+  type: string;
+  host: string;
+  username: string;
+  password: string;
+  database: string;
+  port: number;
+  logging: boolean;
+  entities: string[];
+  migrationsRun: boolean;
+  synchronize: boolean;
+}
