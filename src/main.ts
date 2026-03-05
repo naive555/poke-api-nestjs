4f2bbbfa7efbe15c19ffb431c070a4832ec2196c
Non Ninkham <non.n@sms2pro.com>
2026-03-05 10:27:35 +0700

    refactor: improve bootstrap

--- src/main.ts
+++ src/main.ts
@@ -14,40 +14,43 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter() as FastifyAdapter,
-  );
-
-  await app.register(compression);
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
+    new FastifyAdapter(),
   );
 
   const configService = app.get(ConfigService);
-
   const logger = new Logger('NestApplication');
+
   const isCorsEnabled = configService.get<boolean>('common.cors');
   const name = configService.get<string>('common.name');
   const version = configService.get<string>('common.version');
   const environment = configService.get<string>('common.environment');
-  const port = configService.get<number>('common.port');
+  const port = configService.get<number>('common.port') ?? 3001;
 
   if (isCorsEnabled) {
     await app.register(cors, {
-      origin: '*',
+      origin: configService.get<string | string[]>('common.corsDomains'),
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     });
   }
+
+  await app.register(compression);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(port, '0.0.0.0');
   logger.log(`${name} - ${version}`);
   logger.log(`On ${environment} environment`);
-  logger.log(`Enable CORS ${isCorsEnabled}`);
-  logger.log(`Started on port ${port}`);
+  logger.log(`Enable CORS: ${isCorsEnabled}`);
+  logger.log(`Started on port: ${port}`);
 }
 
 bootstrap();
